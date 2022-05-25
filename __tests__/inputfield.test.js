ba192c406ebaa8538ef17660a195a10886f28621
jagu-ms <mohamedseddiki20@gmail.com>
2022-05-25 21:39:20 +0100

    The 'only' method

--- __tests__/inputfield.test.js
+++ __tests__/inputfield.test.js
@@ -1,5 +1,20 @@
 const inputField = require("../inputfield");
 
+// The "only" method tests only this method
+// The "only" method on describe
+describe.only("Check the inputField function", () => {
+  it("If no argument was inputed", () => {
+    expect(inputField()).toBe("Unknown")
+  })
+  it("Check if the name is without spaces", () => {
+    expect(inputField(" Hadjer ")).toBe("Hadjer")
+  })
+  // The "only" method on the "it" method
+  it.only("If no argument was inputed", () => {
+    expect(inputField("mohamedseddiki_jagu")).toBe("mohamedsed")
+  })
+})
+
 describe("Check the inputField function", () => {
   it("If no argument was inputed", () => {
     expect(inputField()).toBe("Unknown")
